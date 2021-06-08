e6a36bbcffa1caa26a1239141140742de6fbdd46
hoffmann <silas.hoffmann@dps.de>
2021-06-08 15:44:33 +0200

    init

---
+++ test-receiver/src/main/java/com/example/testreceiver/DefaultController.java
@@ -0,0 +1,54 @@
+package com.example.testreceiver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String health() {
+        String msg = "component is healthy";
+        log.info(msg);
+        return msg;
+    }
+
+    @RequestMapping("/receive")
+    public void receiveGet() {
+        log.info("received get alert");
+        callScaleApi();
+    }
+
+    @PostMapping("/receive")
+    public void receivePost() {
+        log.info("received post alert");
+        callScaleApi();
+    }
+
+    private void callScaleApi() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://localhost:8080/v1/scale-service";
+        String requestJson = "{" +
+                "\"groupLabels\": " +
+                "{\"scale\": \"up\", " +
+                "\"service\": \"vossibility_helloworld\"}" +
+                "}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(requestJson,headers);
+        String answer = restTemplate.postForObject(url, entity, String.class);
+        System.out.println(answer);
+    }
+
+}
---
+++ test-receiver/src/test/java/com/example/testreceiver/TestreceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestreceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
