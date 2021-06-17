c3f5395bb1bcef7d721b40a7971b7270518912df
hoffmann <silas.hoffmann@dps.de>
2021-06-17 10:14:49 +0200

    updated build script

--- clean-build-run.sh
+++ clean-build-run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
+STACK_DIR=stack
+export $(cat $STACK_DIR/.env | xargs)
+
 echo 'create network'
 docker network create -d overlay scaler
 
 echo 'build images'
-docker-compose -f stack/docker-compose.yml build
+docker-compose -f $STACK_DIR/docker-compose.yml build
 
 ./start-stack-services.sh
 
