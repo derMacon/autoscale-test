72081b73964d9a2440da9000f923e7125802529c
hoffmann <silas.hoffmann@dps.de>
2021-06-18 16:36:54 +0200

    further reverts

--- stack/scaler-proxy/src/main/java/dps/hoffmann/proxy/model/ScalingInterval.java
+++ stack/scaler-proxy/src/main/java/dps/hoffmann/proxy/model/ScalingInterval.java
@@ -1,6 +1,6 @@
 package dps.hoffmann.proxy.model;
 
 public enum ScalingInterval {
-    SMALL_SCALE_CNT, MEDIUM_SCALE_CNT, LARGE_SCALE_CNT,
+    SMALL_SCALE_CNT, MEDIUM_SCALE_CNT, LARGE_SCALE_CNT
 //    DIFF_LARGE_MEDIUM, DIFF_MEDIUM_SMALL
 }
