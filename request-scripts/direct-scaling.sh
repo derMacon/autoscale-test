f94b70a1b06ce8e335948660b9555aeefb7b5960
hoffmann <silas.hoffmann@dps.de>
2021-06-27 13:06:07 +0200

    updated request scripts

--- request-scripts/direct-scaling.sh
+++ request-scripts/direct-scaling.sh
@@ -4,10 +4,11 @@ echo 'direct scaling benchmarks'
 
 PROCESSED_MSG_CNT=1;
 OVERALL_MSG_CNT=$(( 2 * $1 * $2 ))
+HOST="10.21.2.221"
 
 request() {
 	echo "$PROCESSED_MSG_CNT/$OVERALL_MSG_CNT - scaling request: {scalingOffset: $1, repetition: $2/$3, service: $4}"
-	curl "http://localhost:9245/manual-scale?additionalCnt=$1&service=$4"
+	curl "http://$HOST:9245/manual-scale?additionalCnt=$1&service=$4"
 }
 
 node_request() {
