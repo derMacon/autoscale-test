68368612eed7df094087b92ed297b57e4f6312c2
derMacon <silash169@gmail.com>
2021-09-21 12:00:00 +0200

    added quarkus project

---
+++ stack/quarkus-consumer/src/main/java/org/acme/jms/Gift.java
@@ -0,0 +1,33 @@
+package org.acme.jms;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Gift {
+    private Long id;
+    private String name;
+
+    @Id
+    @SequenceGenerator(name = "giftSeq", sequenceName = "gift_id_seq", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "giftSeq")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
---
+++ stack/quarkus-consumer/src/main/java/org/acme/jms/PriceConsumer.java
@@ -0,0 +1,36 @@
+package org.acme.jms;
+
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * A bean consuming data from the "request" AMQP queue and giving out a random quote.
+ * The result is pushed to the "quotes" AMQP queue.
+ */
+@ApplicationScoped
+public class PriceConsumer {
+
+    private Random random = new Random();
+
+    @Inject
+    EntityManager em;
+
+    @Incoming("requests")       // <1>
+    @Blocking                   // <3>
+//    @Transactional
+    public void process(String quoteRequest) throws InterruptedException {
+        // simulate some hard working task
+        Thread.sleep(200);
+        System.out.println("req: " + quoteRequest);
+//        Gift gift = new Gift();
+//        gift.setName(UUID.randomUUID().toString());
+//        em.persist(gift);
+    }
+}
