93bee3f01b735b42c7b9a5ece78265cb1db636d9
hoffmann <silas.hoffmann@dps.de>
2021-09-28 13:46:59 +0200

    modified request translation mapper

--- stack/scaler-proxy/src/main/java/dps/hoffmann/proxy/model/RequestMapper.java
+++ stack/scaler-proxy/src/main/java/dps/hoffmann/proxy/model/RequestMapper.java
@@ -51,7 +51,8 @@ public class RequestMapper {
 
         SPRING_BASELINE(SPRING, "spring_baseline", UP, CL0),
         NODE_BASELINE(NODE, "node_baseline", UP, CL0),
-        
+        QUARKUS_BASELINE(NODE, "node_baseline", UP, CL0),
+
         SPRING_QL1_CL0(SPRING, "spring_ql1_cl0", UP, DIFF_CL0_CL1),
         SPRING_QL2_CL0(SPRING, "spring_ql2_cl0", UP, DIFF_CL0_CL2),
         SPRING_QL3_CL0(SPRING, "spring_ql3_cl0", UP, DIFF_CL0_CL3),
@@ -67,6 +68,22 @@ public class RequestMapper {
         SPRING_QL0_CL3(SPRING, "spring_ql0_cl3", DOWN, DIFF_CL0_CL3),
         SPRING_QL1_CL3(SPRING, "spring_ql1_cl3", DOWN, DIFF_CL1_CL3),
         SPRING_QL2_CL3(SPRING, "spring_ql2_cl3", DOWN, DIFF_CL2_CL3),
+
+        QUARKUS_QL1_CL0(QUARKUS, "quarkus_ql1_cl0", UP, DIFF_CL0_CL1),
+        QUARKUS_QL2_CL0(QUARKUS, "quarkus_ql2_cl0", UP, DIFF_CL0_CL2),
+        QUARKUS_QL3_CL0(QUARKUS, "quarkus_ql3_cl0", UP, DIFF_CL0_CL3),
+
+        QUARKUS_QL0_CL1(QUARKUS, "quarkus_ql0_cl1", DOWN, DIFF_CL0_CL1),
+        QUARKUS_QL2_CL1(QUARKUS, "quarkus_ql2_cl1", UP, DIFF_CL1_CL2),
+        QUARKUS_QL3_CL1(QUARKUS, "quarkus_ql3_cl1", UP, DIFF_CL1_CL3),
+
+        QUARKUS_QL0_CL2(QUARKUS, "quarkus_ql0_cl2", DOWN, DIFF_CL0_CL2),
+        QUARKUS_QL1_CL2(QUARKUS, "quarkus_ql1_cl2", DOWN, DIFF_CL1_CL2),
+        QUARKUS_QL3_CL2(QUARKUS, "quarkus_ql3_cl2", UP, DIFF_CL2_CL3),
+
+        QUARKUS_QL0_CL3(QUARKUS, "quarkus_ql0_cl3", DOWN, DIFF_CL0_CL3),
+        QUARKUS_QL1_CL3(QUARKUS, "quarkus_ql1_cl3", DOWN, DIFF_CL1_CL3),
+        QUARKUS_QL2_CL3(QUARKUS, "quarkus_ql2_cl3", DOWN, DIFF_CL2_CL3),
 
         NODE_QL1_CL0(NODE, "node_ql1_cl0", UP, DIFF_CL0_CL1),
         NODE_QL2_CL0(NODE, "node_ql2_cl0", UP, DIFF_CL0_CL2),
